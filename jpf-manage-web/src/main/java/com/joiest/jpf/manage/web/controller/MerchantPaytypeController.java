c783a317f2304842de8a0db34c059b475591618f
mazeqiang <mazeqiang@joiest.com>
2018-04-02 16:00:51 +0800

    manage商户信息管理业务类

---
+++ jpf-manage-web/src/main/java/com/joiest/jpf/manage/web/controller/MerchantPaytypeController.java
@@ -0,0 +1,9 @@
+package com.joiest.jpf.manage.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/merchant/paytype")
+public class MerchantPaytypeController {
+}
