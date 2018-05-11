f2cee4d3a1d1336ac50dac10d1812102dc4d26d9
lixiaofei <lxf23@163.com>
2018-05-11 09:29:22 +0800

    完善接收银联退款返回信息接口 ver7

--- jpf-manage-web/src/main/java/com/joiest/jpf/manage/web/constant/ManageConstants.java
+++ jpf-manage-web/src/main/java/com/joiest/jpf/manage/web/constant/ManageConstants.java
@@ -4,6 +4,6 @@ public class ManageConstants {
 
     public static final String USERINFO_SESSION = "manage-userInfo";
 
-    public static final String REFUND_URL = "https://api.7shengqian.com/index.php?r=YinjiaStage/PurchaseRefund";
+    public static final String REFUND_URL = "http://testapi.7shengqian.com/index.php?r=YinjiaStage/PurchaseRefund";
 
 }
