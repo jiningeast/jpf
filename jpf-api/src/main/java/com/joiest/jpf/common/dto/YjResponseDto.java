636da7a1296f73d6a2463e56a92e7a2903293b53
wlliang666 <27275293@qq.com>
2018-05-19 20:16:14 +0800

    前台返回dto

--- jpf-api/src/main/java/com/joiest/jpf/common/dto/YjResponseDto.java
+++ jpf-api/src/main/java/com/joiest/jpf/common/dto/YjResponseDto.java
@@ -15,46 +15,7 @@ public class YjResponseDto {
      */
     private String Info = "SUCCESS";
 
-//    /**
-//     * 状态码 默认"10000"为正常的响应码, 代表操作完成, 具体交易状态, 以交易业务状态码为准
-//     */
-//    private String front_retCode_success = "10000";
-//
-//    /**
-//     * 失败
-//     */
-//    private String front_retCode_fail = "10008";
-//
-//    /**
-//     * 未登录
-//     */
-//    private String front_retCode_notLogin = "10007";
-//
-//    /**
-//     * 短信验证失败
-//     */
-//    private String front_retCode_smsValiFail = "10005";
-//
-//    /**
-//     * 商户信息获取失败
-//     */
-//    private String front_retCode_merInfoFail = "10009";
-//
-//    /**
-//     * 商户未绑定储蓄卡
-//     */
-//    private String front_retCode_notBindCard = "10006";
-//
-//    /**
-//     * 用户未签约
-//     */
-//    private String front_retCode_userNotSign = "10010";
-//
-//    /**
-//     * 用户已签约
-//     */
-//    private String front_retCode_userAlreadysign = "10011";
-
+    private Object data;
 
     public String getCode() {
         return Code;
@@ -70,5 +31,13 @@ public class YjResponseDto {
 
     public void setInfo(String info) {
         Info = info;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
     }
 }
