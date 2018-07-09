95d9e9cfce43d94aa237b1a1f736235a5f65ed07
lixiaofei <lxf23@163.com>
2018-07-09 14:39:03 +0800

    上传打款任务相关功能v0.2

--- jpf-manage-web/src/main/java/com/joiest/jpf/manage/web/controller/CloudTaskController.java
+++ jpf-manage-web/src/main/java/com/joiest/jpf/manage/web/controller/CloudTaskController.java
@@ -12,7 +12,10 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
+
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Controller
@@ -55,17 +58,52 @@ public class CloudTaskController {
 
     /**
      * 新任务提交操作
+     * pay_cloud_company_staff 企业的自由职业者信息表
+     * pay_cloud_staff_banks 自由职业者的银行卡信息表
+     * pay_cloud_df_money 云账户充值记录表
      */
     @RequestMapping("/submitTask")
     @ResponseBody
-    public String submitTask(String company_id, String company_name, @RequestParam("uploadfile") MultipartFile uploadfile) throws Exception{
+    public Map<String, Object> submitTask(String company_id, String company_name, @RequestParam("uploadfile") MultipartFile uploadfile) throws Exception{
+        // 保存excel文件
+
+        // 判断excel数据正确完整性
+
+
         // 解析xml
         ExcelDealUtils excelDealUtils = new ExcelDealUtils();
         Map<Object,Object> map = excelDealUtils.getImportExcel(uploadfile.getInputStream(), uploadfile.getOriginalFilename());
-        String test = "wow";
+        List<Object> list = new ArrayList<>(map.values());
+
+        // 展示给前台
+        Map<String,Object> responseMap = new HashMap<>();
+        responseMap.put("total",map.size()-4);
+        responseMap.put("rows",list);
+
+        return responseMap;
 
         // 上传
+    }
 
-        return "";
+    /**
+     * 获取上传的excel文件数据
+     */
+    @RequestMapping("/getExcelData")
+    @ResponseBody
+    public Map<String,Object> getExcelData(MultipartFile uploadfile) throws Exception{
+        // 解析xml
+        ExcelDealUtils excelDealUtils = new ExcelDealUtils();
+        Map<Object,Object> map = excelDealUtils.getImportExcel(uploadfile.getInputStream(), uploadfile.getOriginalFilename());
+        List<Object> list = new ArrayList<>(map.values());
+
+        /*for (Object obj:list){
+
+        }*/
+
+        Map<String,Object> responseMap = new HashMap<>();
+        responseMap.put("total",map.size()-4);
+        responseMap.put("rows",list);
+
+        return responseMap;
     }
 }
