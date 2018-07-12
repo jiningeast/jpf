7813d32f489b221fd8a63cc01b207359b01c0dea
mzqiang <mazeqiang@joiest.com>
2018-07-12 16:28:49 +0800

    pay_cloud_recharge 状态的枚举类型 已添加

--- jpf-common/src/main/java/com/joiest/jpf/common/constant/EnumConstants.java
+++ jpf-common/src/main/java/com/joiest/jpf/common/constant/EnumConstants.java
@@ -70,6 +70,66 @@ public abstract class EnumConstants {
         public final Byte value(){return value;}
     }
 
+    /**
+     * 充值状态（需求）
+     */
+    public enum RechargeStatus{
+        APPLYING((byte)1,"申请中(需求提交，合同待审核first) "),
+        AUDIT((byte)2,"已审核(已签约，待上传付款凭证)"),
+        PAY((byte)3,"已支付(已上传凭证)"),
+        RECHARGE_AND_TICKET_OPENING((byte)4,"已充值开票中(第二次审核)"),
+        RECHARGE_AND_TICKET((byte)5,"已充值已开票"),
+        DELIVERED((byte)6,"已发货"),
+        COMPLETE((byte)7,"已完成(客户收到发票)"),
+        CANCEL((byte)0,"已取消")
+        ;
+
+        private Byte value;
+
+        private String desc;
+
+        RechargeStatus(Byte value,String desc){
+            this.value = value;
+            this.desc = desc;
+        }
+
+        public final Byte value() {
+            return value;
+        }
+
+        public final String desc() {
+            return desc;
+        }
+
+    }
+
+    /**
+     * 充值-确认需求状态
+     */
+    public enum RechargePactStatus{
+        UNCONFIRMED((byte)1,"默认，未确认需求"),
+        CONFIRMED((byte)2,"已经确认需求")
+        ;
+
+        private Byte value;
+
+        private String desc;
+
+        RechargePactStatus(Byte value,String desc){
+            this.value = value;
+            this.desc = desc;
+        }
+
+        public final Byte value() {
+            return value;
+        }
+
+        public final String desc() {
+            return desc;
+        }
+
+    }
+
 
 
 }
