84156b1ce9c65f829f5811e5e6ec36bb70b0e389
27275293@qq.com <wangliang666>
2018-07-13 14:03:24 +0800

    pay_cloud_df_money 自定义po添加字段

--- jpf-domain/src/main/java/com/joiest/jpf/common/custom/PayCloudCompactStaffInterfaceCustom.java
+++ jpf-domain/src/main/java/com/joiest/jpf/common/custom/PayCloudCompactStaffInterfaceCustom.java
@@ -50,6 +50,31 @@ public class PayCloudCompactStaffInterfaceCustom implements Serializable {
      */
     private Date updated;
 
+    /**
+     * 合同模板ID
+     */
+    private Long compactid;
+
+    /**
+     * 服务内容id
+     */
+    private Integer ticketid;
+
+    /**
+     * 服务内容
+     */
+    private String ticketcontent;
+
+    /**
+     * 项目名称id
+     */
+    private String entryid;
+
+    /**
+     * 项目名称
+     */
+    private String entryname;
+
     //============== 自定义添加 Begin ===================
     /**
      * 收款人
@@ -168,5 +193,45 @@ public class PayCloudCompactStaffInterfaceCustom implements Serializable {
 
     public void setUpdated(Date updated) {
         this.updated = updated;
+    }
+
+    public Long getCompactid() {
+        return compactid;
+    }
+
+    public void setCompactid(Long compactid) {
+        this.compactid = compactid;
+    }
+
+    public Integer getTicketid() {
+        return ticketid;
+    }
+
+    public void setTicketid(Integer ticketid) {
+        this.ticketid = ticketid;
+    }
+
+    public String getTicketcontent() {
+        return ticketcontent;
+    }
+
+    public void setTicketcontent(String ticketcontent) {
+        this.ticketcontent = ticketcontent;
+    }
+
+    public String getEntryid() {
+        return entryid;
+    }
+
+    public void setEntryid(String entryid) {
+        this.entryid = entryid;
+    }
+
+    public String getEntryname() {
+        return entryname;
+    }
+
+    public void setEntryname(String entryname) {
+        this.entryname = entryname;
     }
 }
