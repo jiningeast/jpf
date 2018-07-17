97251f2e4772d9a2f8c03aeadd1debb27747eaf5
27275293@qq.com <wangliang666>
2018-07-17 10:55:21 +0800

    日期修正

--- jpf-common/src/main/java/com/joiest/jpf/common/util/ToolUtils.java
+++ jpf-common/src/main/java/com/joiest/jpf/common/util/ToolUtils.java
@@ -160,6 +160,11 @@ public class ToolUtils {
     public static Map<String,String> getMonthFirstAndEndSenond(int year, int month)
     {
         Calendar calendar = Calendar.getInstance();
+
+        calendar.set(Calendar.YEAR, year);
+
+        calendar.set(Calendar.MONTH, month-1);
+
         calendar.set(Calendar.DAY_OF_MONTH, 1);
         //将小时至0
         calendar.set(Calendar.HOUR_OF_DAY, 0);
