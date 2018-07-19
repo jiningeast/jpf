17276b3e7a44318348846a7182e6ac3260ff18c6
zhangxiaoyun <zhangxiaoyun@joiest.com>
2018-07-19 13:55:04 +0800

    代付冲突解决

--- jpf-api/src/main/java/com/joiest/jpf/facade/CloudDfOrderInterfaceServiceFacade.java
+++ jpf-api/src/main/java/com/joiest/jpf/facade/CloudDfOrderInterfaceServiceFacade.java
@@ -16,11 +16,11 @@ public interface CloudDfOrderInterfaceServiceFacade {
     public int addOrder(String batchid, String dfid, List<CloudDfMoneyInterfaceInfo> list, String batchid_self);
 
     /**
-<<<<<<< HEAD
      *根据外来单号查询代付数据  request_orderid
      * **/
     public CloudDfMoneyInterfaceInfo getDfOrderByRequestOrderid(String request_orderid);
-=======
+
+    /*
      * 获取批次下的代付信息
      * @param batchid 批次号
      * @return
@@ -33,5 +33,5 @@ public interface CloudDfOrderInterfaceServiceFacade {
      * @return
      */
     public int updateDfOrderStatus(CloudDfOrderInterfaceInfo info);
->>>>>>> 94a18e5ddd51051becac646992cb52f1a059b8d4
+
 }
