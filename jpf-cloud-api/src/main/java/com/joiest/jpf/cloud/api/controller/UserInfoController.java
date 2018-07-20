56525569b283dc0f97f89a8d68cb8fa410495540
zhangxiaoyun <zhangxiaoyun@joiest.com>
2018-07-20 15:28:29 +0800

    大小写

--- jpf-cloud-api/src/main/java/com/joiest/jpf/cloud/api/controller/UserInfoController.java
+++ jpf-cloud-api/src/main/java/com/joiest/jpf/cloud/api/controller/UserInfoController.java
@@ -224,7 +224,7 @@ public class UserInfoController {
 
             return ToolUtils.toJsonBase64(JpfInterfaceErrorInfo.FAIL.getCode(), "Error",null);
         }
-        idCard = Base64CustomUtils.base64Decoder(idCard);
+        idCard = Base64CustomUtils.base64Decoder(idCard).toUpperCase();
         verificate = Base64CustomUtils.base64Decoder(verificate);
 
         //获取员工信息
