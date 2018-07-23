347e17bef4ababac58367c0f3ecfc57cdacb2211
JiaYamei <905412150@qq.com>
2018-07-23 14:53:50 +0800

    商户信息展示 by jym

--- jpf-cloud-api/src/main/java/com/joiest/jpf/cloud/api/controller/merchInfoController.java
+++ jpf-cloud-api/src/main/java/com/joiest/jpf/cloud/api/controller/merchInfoController.java
@@ -38,7 +38,6 @@ public class merchInfoController {
     @RequestMapping(value = "/getMerchantInfo", method = RequestMethod.POST, produces = "application/json;charset=utf-8")
     @ResponseBody
     public String getMerchantInfo(HttpServletRequest request) {
-
         logger.info("token={}", request.getHeader("token"));
         String token = request.getHeader("token");
         CloudEmployeeInfo cloudEmployeeInfo = cloudEmployeeServiceFacade.companyIsLogin(token);
