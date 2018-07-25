fb89317d47897ee2b4ba1e8cc46cd1d02980c3f6
zhangxiaoyun <zhangxiaoyun@joiest.com>
2018-07-25 14:32:41 +0800

    跨域调试

--- jpf-cloud-api/src/main/java/com/joiest/jpf/cloud/api/controller/CompanyInfoController.java
+++ jpf-cloud-api/src/main/java/com/joiest/jpf/cloud/api/controller/CompanyInfoController.java
@@ -349,9 +349,9 @@ public class CompanyInfoController {
         String originHeader = httpRequest.getHeader("Origin");
         //response.setHeader("Access-Control-Allow-Origin", "*");
         //response.setHeader("Access-Control-Allow-Origin", originHeader);
-        response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, accept, content-type, token,X-Auth-Token");
-        response.setHeader("Access-Control-Allow-Methods", "GET, HEAD, POST, PUT, DELETE, TRACE, OPTIONS, PATCH");
-        response.setHeader("Access-Control-Allow-Credentials", "true");
+        //response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, accept, content-type, token,X-Auth-Token");
+        //response.setHeader("Access-Control-Allow-Methods", "GET, HEAD, POST, PUT, DELETE, TRACE, OPTIONS, PATCH");
+        //response.setHeader("Access-Control-Allow-Credentials", "true");
         //response.setHeader("Access-Control-Allow-Method", "POST,OPTIONS");
         //response.setHeader("token", originHeader);
 
