e34f61cd6a701b9641d72ff2c83124c7e2c8955c
zhangxiaoyun <zhangxiaoyun@joiest.com>
2018-07-25 13:38:13 +0800

    域名限制修改

--- jpf-cloud-api/src/main/java/com/joiest/jpf/cloud/api/controller/CompanyInfoController.java
+++ jpf-cloud-api/src/main/java/com/joiest/jpf/cloud/api/controller/CompanyInfoController.java
@@ -346,11 +346,11 @@ public class CompanyInfoController {
     public void beforeAction(HttpServletRequest httpRequest, HttpServletResponse response)
     {
         // 跨域
-        //String originHeader = httpRequest.getHeader("Origin");
-        response.setHeader("Access-Control-Allow-Origin", "*");
+        String originHeader = httpRequest.getHeader("Origin");
+        //response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Origin", originHeader);
         response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, accept, content-type, token,X-Auth-Token");
         response.setHeader("Access-Control-Allow-Methods", "GET, HEAD, POST, PUT, DELETE, TRACE, OPTIONS, PATCH");
-       // response.setHeader("Access-Control-Allow-Origin", originHeader);
         response.setHeader("Access-Control-Allow-Credentials", "true");
         //response.setHeader("Access-Control-Allow-Method", "POST,OPTIONS");
         //response.setHeader("token", originHeader);
