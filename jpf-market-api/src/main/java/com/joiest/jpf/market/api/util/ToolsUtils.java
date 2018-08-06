534c17e56bdc794df1f9a9b7b289eb956c15ff1a
wangliang <wangliang666>
2018-08-06 11:32:18 +0800

    云市场api模块添加

---
+++ jpf-market-api/src/main/java/com/joiest/jpf/market/api/util/ToolsUtils.java
@@ -0,0 +1,29 @@
+package com.joiest.jpf.market.api.util;
+
+import java.util.Random;
+
+public class ToolsUtils {
+
+    /**
+     * 生成一个唯一数
+     * */
+    public static String createOrderid(){
+
+        int pre = getRandomInt(100,999);
+        int last = getRandomInt(100,999);
+        String middle = String.valueOf(System.currentTimeMillis());
+        middle = middle.substring(3,middle.length());
+
+        return ""+pre+middle+last;
+    }
+    /*
+     *   生成指定范围内的随机整数
+     **/
+    public static int getRandomInt(int min, int max){
+
+        Random random = new Random();
+        int randomInt = random.nextInt(max)%(max-min+1) + min;
+
+        return randomInt;
+    }
+}
