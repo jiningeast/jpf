7b5ef7e73de56a7cad92473164e23dcf77c3304b
zhangxiaoyun <zhangxiaoyun@joiest.com>
2018-08-08 13:32:30 +0800

    微信信息修改

--- jpf-api/src/main/java/com/joiest/jpf/facade/WeixinMapServiceFacade.java
+++ jpf-api/src/main/java/com/joiest/jpf/facade/WeixinMapServiceFacade.java
@@ -2,10 +2,23 @@ package com.joiest.jpf.facade;
 
 import com.joiest.jpf.entity.WeixinMapInfo;
 
+import java.util.Map;
+
 public interface WeixinMapServiceFacade {
 
     /*
      * 公众号信息
      * */
     public WeixinMapInfo getWeixinMpByEncrypt(String encrypt);
+
+    /*
+    * 更新公众号信息
+    * */
+    public int upWeixinMpByEncrypt(String encrypt, Map<String,String> res);
+
+    /**
+     * 获取access_token
+     * @param weixinMapInfo 公众号信息
+     * */
+    public String getAccessToken(WeixinMapInfo weixinMapInfo);
 }
