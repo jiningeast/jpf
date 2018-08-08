d1f3d7000814ab717c42ca4e69c6b4a68b8f6645
cailei <1174355934@qq.com>
2018-08-08 13:45:10 +0800

    欣券激活接口和激活列表接口

--- jpf-service/src/main/java/com/joiest/jpf/facade/impl/ShopCouponActiveInterfaceServiceFacadeImpl.java
+++ jpf-service/src/main/java/com/joiest/jpf/facade/impl/ShopCouponActiveInterfaceServiceFacadeImpl.java
@@ -48,7 +48,54 @@ public class ShopCouponActiveInterfaceServiceFacadeImpl implements ShopCouponAct
         GetUserCouponActiveInterfaceResponse response = new GetUserCouponActiveInterfaceResponse();
         response.setList(resultList);
         int count = payShopCouponActiveMapper.countByExample(example);
-        response.setCount(resultList.size());
+        response.setCount(count);
+        response.setDouTotal(dou_able);
+        return response;
+    }
+    @Override
+    public GetUserCouponActiveInterfaceResponse getUserCouponListAndPage(String uid,String pageNo,String pageSize ) {
+        if ( StringUtils.isBlank(uid) )
+        {
+            return null;
+        }
+
+        PayShopCouponActiveExample example = new PayShopCouponActiveExample();
+
+        if(StringUtils.isBlank(pageNo) || pageNo==null){
+            example.setPageNo(1);
+        }else {
+            example.setPageNo(Long.parseLong(pageNo));
+        }
+        if(StringUtils.isBlank(pageSize) || pageSize==null){
+            example.setPageSize(10);
+        }else{
+            example.setPageSize(Long.parseLong(pageSize));
+        }
+
+        example.setOrderByClause("addtime ASC");
+        PayShopCouponActiveExample.Criteria c = example.createCriteria();
+        c.andCustomerIdEqualTo(uid);
+        c.andTypeEqualTo("0");
+
+        List<PayShopCouponActive> list = payShopCouponActiveMapper.selectByExample(example);
+        if ( list.isEmpty() || list == null )
+        {
+            return null;
+        }
+
+        List<ShopCouponActiveInterfaceInfo> resultList = new ArrayList<>();
+        int dou_able = 0;
+        for (PayShopCouponActive one : list)
+        {
+            ShopCouponActiveInterfaceInfo info = new ShopCouponActiveInterfaceInfo();
+            BeanCopier beanCopier = BeanCopier.create(PayShopCouponActive.class, ShopCouponActiveInterfaceInfo.class, false);
+            beanCopier.copy(one, info, null);
+            resultList.add(info);
+            dou_able += one.getDou();
+        }
+        GetUserCouponActiveInterfaceResponse response = new GetUserCouponActiveInterfaceResponse();
+        response.setList(resultList);
+        int count = payShopCouponActiveMapper.countByExample(example);
         response.setCount(count);
         response.setDouTotal(dou_able);
         return response;
