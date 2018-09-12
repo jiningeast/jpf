dec4a016c236d86350f0515988951a5606d500b8
lixiaofei <lxf23@163.com>
2018-09-12 18:01:37 +0800

    新建bargainOrder和bargainRequest两个表的facade和facadeImpl

---
+++ jpf-service/src/main/java/com/joiest/jpf/facade/impl/ShopBargainRequestServiceFacadeImpl.java
@@ -0,0 +1,7 @@
+package com.joiest.jpf.facade.impl;
+
+import com.joiest.jpf.facade.ShopBargainRequestServiceFacade;
+
+public class ShopBargainRequestServiceFacadeImpl implements ShopBargainRequestServiceFacade {
+
+}
