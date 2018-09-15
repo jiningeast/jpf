0cdbf3985cba396ab42dcf2a0a0dda5ba4030c85
suozhenhua <1653633830@qq.com>
2018-09-15 16:11:58 +0800

    我是买家申请接口及查询接口功能

--- jpf-api/src/main/java/com/joiest/jpf/facade/ShopBargainRequestServiceFacade.java
+++ jpf-api/src/main/java/com/joiest/jpf/facade/ShopBargainRequestServiceFacade.java
@@ -1,8 +1,12 @@
 package com.joiest.jpf.facade;
 
 import com.joiest.jpf.common.dto.JpfResponseDto;
+import com.joiest.jpf.common.po.PayShopBargainRequest;
+import com.joiest.jpf.dto.GetShopBargainOrderRequest;
 import com.joiest.jpf.dto.GetShopBargainRequestRequest;
 import com.joiest.jpf.dto.GetShopBargainRequestResponse;
+
+import java.util.List;
 
 public interface ShopBargainRequestServiceFacade {
     /**
@@ -15,5 +19,15 @@ public interface ShopBargainRequestServiceFacade {
      * 收购开启和关闭
      */
     public JpfResponseDto delShopBargain(String merchNo, int type);
+
+    /*
+     * 添加买家回收信息
+     * */
+    public JpfResponseDto add(GetShopBargainRequestRequest request);
+
+    /*
+     * 查询当前买家 用户发布信息
+     * */
+    public List<PayShopBargainRequest> getOne(GetShopBargainRequestRequest request);
 
 }
