0e671fcf005a027ce2fd5f0bb479877f43ade0dc
lixiaofei <lxf23@163.com>
2018-09-17 21:07:53 +0800

    重测直充4

--- jpf-market-api/src/main/java/com/joiest/jpf/market/api/controller/OrderInfoController.java
+++ jpf-market-api/src/main/java/com/joiest/jpf/market/api/controller/OrderInfoController.java
@@ -102,18 +102,12 @@ public class OrderInfoController {
         }
         response.setUserDou(userInfo.getDou());  //获取用户当前豆数量
         response.setTotalDou(response.getTotalDou());
-        switch (response.getOrderType()){
-            case 1:
-                response.setTypeName("中石化充值");
-                break;
-
-            case 2:
-                response.setTypeName("中石油充值");
-                break;
-
-            case 3:
-                response.setTypeName("话费充值");
-                break;
+        if ( response.getOrderType() == 1 ){
+            response.setTypeName("中石化充值");
+        }else if ( response.getOrderType() == 2 ){
+            response.setTypeName("中石油充值");
+        }else if ( response.getOrderType() == 3 ) {
+            response.setTypeName("话费充值");
         }
 
         return ToolUtils.toJsonBase64(JpfInterfaceErrorInfo.SUCCESS.getCode(), JpfInterfaceErrorInfo.SUCCESS.getDesc(), response);
