98a2a38272dcae02beb91ded4e85e3d49e1413d6
lixiaofei <lxf23@163.com>
2018-10-10 16:45:56 +0800

    后台财务充值审核列表显示商户名称2

--- jpf-service/src/main/java/com/joiest/jpf/facade/impl/CloudCompanyServiceFacadeImpl.java
+++ jpf-service/src/main/java/com/joiest/jpf/facade/impl/CloudCompanyServiceFacadeImpl.java
@@ -719,12 +719,12 @@ public class CloudCompanyServiceFacadeImpl implements CloudCompanyServiceFacade 
         CloudCompanyInfo cloudCompanyInfo = new CloudCompanyInfo();
         if ( payCloudCompanyCustom != null && payCloudCompanyCustom.getAgentNo() != null && StringUtils.isNotBlank(payCloudCompanyCustom.getAgentNo()) ){
             cloudCompanyInfo.setType((byte)1);
+
+            BeanCopier beanCopier = BeanCopier.create(PayCloudCompanyCustom.class,CloudCompanyInfo.class,false);
+            beanCopier.copy(payCloudCompanyCustom,cloudCompanyInfo,null);
         }else{
             cloudCompanyInfo.setType((byte)0);
         }
-
-        BeanCopier beanCopier = BeanCopier.create(PayCloudCompanyCustom.class,CloudCompanyInfo.class,false);
-        beanCopier.copy(payCloudCompanyCustom,cloudCompanyInfo,null);
 
         return cloudCompanyInfo;
     }
