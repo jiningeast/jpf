42461cbe3efcb66bfa63110d38d52a3e5fb947f9
lixiaofei <lxf23@163.com>
2018-10-30 11:02:58 +0800

    新增pay_charge_company_money_stream表 pay_charge_product表新增wn_product_price, of_product_price

--- jpf-domain/src/main/java/com/joiest/jpf/common/po/PayChargeCompanyMoneyStream.java
+++ jpf-domain/src/main/java/com/joiest/jpf/common/po/PayChargeCompanyMoneyStream.java
@@ -26,9 +26,64 @@ public class PayChargeCompanyMoneyStream implements Serializable {
     private String merchNo;
 
     /**
-     * 金额
+     * 订单id 可能是消费订单、充值订单、退款订单
      */
-    private BigDecimal money;
+    private String orderId;
+
+    /**
+     * 订单号 可能是消费订单、充值订单、退款订单
+     */
+    private String orderNo;
+
+    /**
+     * 产品id 消费或退款时的产品id
+     */
+    private String productId;
+
+    /**
+     * 产品名称
+     */
+    private String productName;
+
+    /**
+     * 产品面值
+     */
+    private BigDecimal productValue;
+
+    /**
+     * 产品价格
+     */
+    private BigDecimal productBidPrice;
+
+    /**
+     * 产品标准售价
+     */
+    private Long productSalePrice;
+
+    /**
+     * 产品接口价
+     */
+    private BigDecimal productInterfacePrice;
+
+    /**
+     * 产品数量
+     */
+    private Integer productAmount;
+
+    /**
+     * 总价
+     */
+    private BigDecimal totalMoney;
+
+    /**
+     * 接口类型 0=欧非 1=威能
+     */
+    private Byte interfaceType;
+
+    /**
+     * 接口订单号
+     */
+    private String interfaceOrderNo;
 
     /**
      * 流水类型 1=充值 2=下单 3=退款
@@ -39,6 +94,11 @@ public class PayChargeCompanyMoneyStream implements Serializable {
      * 流水备注
      */
     private String memo;
+
+    /**
+     * 删除标记 0=未删除 1=已删除
+     */
+    private Byte isDel;
 
     /**
      * 添加时间
@@ -84,12 +144,100 @@ public class PayChargeCompanyMoneyStream implements Serializable {
         this.merchNo = merchNo == null ? null : merchNo.trim();
     }
 
-    public BigDecimal getMoney() {
-        return money;
+    public String getOrderId() {
+        return orderId;
     }
 
-    public void setMoney(BigDecimal money) {
-        this.money = money;
+    public void setOrderId(String orderId) {
+        this.orderId = orderId == null ? null : orderId.trim();
+    }
+
+    public String getOrderNo() {
+        return orderNo;
+    }
+
+    public void setOrderNo(String orderNo) {
+        this.orderNo = orderNo == null ? null : orderNo.trim();
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId == null ? null : productId.trim();
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName == null ? null : productName.trim();
+    }
+
+    public BigDecimal getProductValue() {
+        return productValue;
+    }
+
+    public void setProductValue(BigDecimal productValue) {
+        this.productValue = productValue;
+    }
+
+    public BigDecimal getProductBidPrice() {
+        return productBidPrice;
+    }
+
+    public void setProductBidPrice(BigDecimal productBidPrice) {
+        this.productBidPrice = productBidPrice;
+    }
+
+    public Long getProductSalePrice() {
+        return productSalePrice;
+    }
+
+    public void setProductSalePrice(Long productSalePrice) {
+        this.productSalePrice = productSalePrice;
+    }
+
+    public BigDecimal getProductInterfacePrice() {
+        return productInterfacePrice;
+    }
+
+    public void setProductInterfacePrice(BigDecimal productInterfacePrice) {
+        this.productInterfacePrice = productInterfacePrice;
+    }
+
+    public Integer getProductAmount() {
+        return productAmount;
+    }
+
+    public void setProductAmount(Integer productAmount) {
+        this.productAmount = productAmount;
+    }
+
+    public BigDecimal getTotalMoney() {
+        return totalMoney;
+    }
+
+    public void setTotalMoney(BigDecimal totalMoney) {
+        this.totalMoney = totalMoney;
+    }
+
+    public Byte getInterfaceType() {
+        return interfaceType;
+    }
+
+    public void setInterfaceType(Byte interfaceType) {
+        this.interfaceType = interfaceType;
+    }
+
+    public String getInterfaceOrderNo() {
+        return interfaceOrderNo;
+    }
+
+    public void setInterfaceOrderNo(String interfaceOrderNo) {
+        this.interfaceOrderNo = interfaceOrderNo == null ? null : interfaceOrderNo.trim();
     }
 
     public Byte getStatus() {
@@ -106,6 +254,14 @@ public class PayChargeCompanyMoneyStream implements Serializable {
 
     public void setMemo(String memo) {
         this.memo = memo == null ? null : memo.trim();
+    }
+
+    public Byte getIsDel() {
+        return isDel;
+    }
+
+    public void setIsDel(Byte isDel) {
+        this.isDel = isDel;
     }
 
     public Date getAddtime() {
@@ -137,9 +293,21 @@ public class PayChargeCompanyMoneyStream implements Serializable {
         sb.append(", companyId=").append(companyId);
         sb.append(", companyName=").append(companyName);
         sb.append(", merchNo=").append(merchNo);
-        sb.append(", money=").append(money);
+        sb.append(", orderId=").append(orderId);
+        sb.append(", orderNo=").append(orderNo);
+        sb.append(", productId=").append(productId);
+        sb.append(", productName=").append(productName);
+        sb.append(", productValue=").append(productValue);
+        sb.append(", productBidPrice=").append(productBidPrice);
+        sb.append(", productSalePrice=").append(productSalePrice);
+        sb.append(", productInterfacePrice=").append(productInterfacePrice);
+        sb.append(", productAmount=").append(productAmount);
+        sb.append(", totalMoney=").append(totalMoney);
+        sb.append(", interfaceType=").append(interfaceType);
+        sb.append(", interfaceOrderNo=").append(interfaceOrderNo);
         sb.append(", status=").append(status);
         sb.append(", memo=").append(memo);
+        sb.append(", isDel=").append(isDel);
         sb.append(", addtime=").append(addtime);
         sb.append(", updatetime=").append(updatetime);
         sb.append("]");
@@ -166,9 +334,21 @@ public class PayChargeCompanyMoneyStream implements Serializable {
             && (this.getCompanyId() == null ? other.getCompanyId() == null : this.getCompanyId().equals(other.getCompanyId()))
             && (this.getCompanyName() == null ? other.getCompanyName() == null : this.getCompanyName().equals(other.getCompanyName()))
             && (this.getMerchNo() == null ? other.getMerchNo() == null : this.getMerchNo().equals(other.getMerchNo()))
-            && (this.getMoney() == null ? other.getMoney() == null : this.getMoney().equals(other.getMoney()))
+            && (this.getOrderId() == null ? other.getOrderId() == null : this.getOrderId().equals(other.getOrderId()))
+            && (this.getOrderNo() == null ? other.getOrderNo() == null : this.getOrderNo().equals(other.getOrderNo()))
+            && (this.getProductId() == null ? other.getProductId() == null : this.getProductId().equals(other.getProductId()))
+            && (this.getProductName() == null ? other.getProductName() == null : this.getProductName().equals(other.getProductName()))
+            && (this.getProductValue() == null ? other.getProductValue() == null : this.getProductValue().equals(other.getProductValue()))
+            && (this.getProductBidPrice() == null ? other.getProductBidPrice() == null : this.getProductBidPrice().equals(other.getProductBidPrice()))
+            && (this.getProductSalePrice() == null ? other.getProductSalePrice() == null : this.getProductSalePrice().equals(other.getProductSalePrice()))
+            && (this.getProductInterfacePrice() == null ? other.getProductInterfacePrice() == null : this.getProductInterfacePrice().equals(other.getProductInterfacePrice()))
+            && (this.getProductAmount() == null ? other.getProductAmount() == null : this.getProductAmount().equals(other.getProductAmount()))
+            && (this.getTotalMoney() == null ? other.getTotalMoney() == null : this.getTotalMoney().equals(other.getTotalMoney()))
+            && (this.getInterfaceType() == null ? other.getInterfaceType() == null : this.getInterfaceType().equals(other.getInterfaceType()))
+            && (this.getInterfaceOrderNo() == null ? other.getInterfaceOrderNo() == null : this.getInterfaceOrderNo().equals(other.getInterfaceOrderNo()))
             && (this.getStatus() == null ? other.getStatus() == null : this.getStatus().equals(other.getStatus()))
             && (this.getMemo() == null ? other.getMemo() == null : this.getMemo().equals(other.getMemo()))
+            && (this.getIsDel() == null ? other.getIsDel() == null : this.getIsDel().equals(other.getIsDel()))
             && (this.getAddtime() == null ? other.getAddtime() == null : this.getAddtime().equals(other.getAddtime()))
             && (this.getUpdatetime() == null ? other.getUpdatetime() == null : this.getUpdatetime().equals(other.getUpdatetime()));
     }
@@ -184,9 +364,21 @@ public class PayChargeCompanyMoneyStream implements Serializable {
         result = prime * result + ((getCompanyId() == null) ? 0 : getCompanyId().hashCode());
         result = prime * result + ((getCompanyName() == null) ? 0 : getCompanyName().hashCode());
         result = prime * result + ((getMerchNo() == null) ? 0 : getMerchNo().hashCode());
-        result = prime * result + ((getMoney() == null) ? 0 : getMoney().hashCode());
+        result = prime * result + ((getOrderId() == null) ? 0 : getOrderId().hashCode());
+        result = prime * result + ((getOrderNo() == null) ? 0 : getOrderNo().hashCode());
+        result = prime * result + ((getProductId() == null) ? 0 : getProductId().hashCode());
+        result = prime * result + ((getProductName() == null) ? 0 : getProductName().hashCode());
+        result = prime * result + ((getProductValue() == null) ? 0 : getProductValue().hashCode());
+        result = prime * result + ((getProductBidPrice() == null) ? 0 : getProductBidPrice().hashCode());
+        result = prime * result + ((getProductSalePrice() == null) ? 0 : getProductSalePrice().hashCode());
+        result = prime * result + ((getProductInterfacePrice() == null) ? 0 : getProductInterfacePrice().hashCode());
+        result = prime * result + ((getProductAmount() == null) ? 0 : getProductAmount().hashCode());
+        result = prime * result + ((getTotalMoney() == null) ? 0 : getTotalMoney().hashCode());
+        result = prime * result + ((getInterfaceType() == null) ? 0 : getInterfaceType().hashCode());
+        result = prime * result + ((getInterfaceOrderNo() == null) ? 0 : getInterfaceOrderNo().hashCode());
         result = prime * result + ((getStatus() == null) ? 0 : getStatus().hashCode());
         result = prime * result + ((getMemo() == null) ? 0 : getMemo().hashCode());
+        result = prime * result + ((getIsDel() == null) ? 0 : getIsDel().hashCode());
         result = prime * result + ((getAddtime() == null) ? 0 : getAddtime().hashCode());
         result = prime * result + ((getUpdatetime() == null) ? 0 : getUpdatetime().hashCode());
         return result;
