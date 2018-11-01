f0726d3d3cd76467ae049a075daed5e3dcb59ccb
lixiaofei <lxf23@163.com>
2018-11-01 15:25:38 +0800

    资金流水表pay_charge_company_money表product_price字段更改类型

--- jpf-api/src/main/java/com/joiest/jpf/facade/ChargeCompanyMoneyStreamServiceFacade.java
+++ jpf-api/src/main/java/com/joiest/jpf/facade/ChargeCompanyMoneyStreamServiceFacade.java
@@ -2,6 +2,7 @@ package com.joiest.jpf.facade;
 
 import com.joiest.jpf.dto.ChargeCompanyMoneyStreamRequest;
 import com.joiest.jpf.dto.ChargeCompanyMoneyStreamResponse;
+import com.joiest.jpf.entity.ChargeCompanyMoneyStreamInfo;
 
 public interface ChargeCompanyMoneyStreamServiceFacade {
 
@@ -9,5 +10,10 @@ public interface ChargeCompanyMoneyStreamServiceFacade {
      * 获取商户列表
      */
     public ChargeCompanyMoneyStreamResponse getRecords(ChargeCompanyMoneyStreamRequest request);
+
+    /**
+     * 新增流水
+     */
+    public int insRecord(ChargeCompanyMoneyStreamInfo info);
 
 }
