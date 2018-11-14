03f0694b1565513670cfeeb4f448a3b174611dfc
tianzj <tianzhijin@joiest.com>
2018-11-14 10:39:35 +0800

    使用多线程匹配数据，提高匹配效率

---
+++ jpf-service/src/main/java/com/joiest/jpf/facade/impl/ConsumerOrderServiceFacadeImpl.java
@@ -0,0 +1,6 @@
+package com.joiest.jpf.facade.impl;
+
+import com.joiest.jpf.facade.ConsumerOrderServiceFacade;
+
+public class ConsumerOrderServiceFacadeImpl implements ConsumerOrderServiceFacade {
+}
