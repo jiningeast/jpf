85039588861245644f6f2f740a7c5314629f785a
zhouchaowei <zhouchaowei96492@163.com>
2018-11-21 18:59:08 +0800

    新增生活旅游服务商品展示列表接口响应实体类

---
+++ jpf-api/src/main/java/com/joiest/jpf/dto/ShopProductInfoResponse.java
@@ -0,0 +1,34 @@
+package com.joiest.jpf.dto;
+
+public class ShopProductInfoResponse {
+    
+    private String title;
+    
+    private String moneyscope;
+    
+    private String imgUrl;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getMoneyscope() {
+        return moneyscope;
+    }
+
+    public void setMoneyscope(String moneyscope) {
+        this.moneyscope = moneyscope;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+}
