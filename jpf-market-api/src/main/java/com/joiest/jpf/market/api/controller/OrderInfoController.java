4e96688334d7fc23c9750d9c8dacbb70e913c02d
zhouchaowei <zhouchaowei96492@163.com>
2018-11-30 16:55:58 +0800

    修改商品详情页商品类型读取及订单详情页读取

--- jpf-market-api/src/main/java/com/joiest/jpf/market/api/controller/OrderInfoController.java
+++ jpf-market-api/src/main/java/com/joiest/jpf/market/api/controller/OrderInfoController.java
@@ -106,15 +106,15 @@ public class OrderInfoController {
         response.setUserDou(userInfo.getDou());  //获取用户当前豆数量
         response.setCurrentSystemTime(new Date()); // 传当前系统时间
         response.setTotalDou(response.getTotalDou());
-        if ( response.getOrderType() == 1 ){
-            response.setTypeName("中石化充值");
-        }else if ( response.getOrderType() == 2 ){
-            response.setTypeName("中石油充值");
-        }else if ( response.getOrderType() == 3 ) {
-            response.setTypeName("话费充值");
-        }else if( response.getOrderType() == 4){
-            response.setTypeName("携程商品");
-        }
+//        if ( response.getOrderType() == 1 ){
+//            response.setTypeName("中石化充值");
+//        }else if ( response.getOrderType() == 2 ){
+//            response.setTypeName("中石油充值");
+//        }else if ( response.getOrderType() == 3 ) {
+//            response.setTypeName("话费充值");
+//        }else if( response.getOrderType() == 4){
+//            response.setTypeName("携程商品");
+//        }
 
         return ToolUtils.toJsonBase64(JpfInterfaceErrorInfo.SUCCESS.getCode(), JpfInterfaceErrorInfo.SUCCESS.getDesc(), response);
     }
