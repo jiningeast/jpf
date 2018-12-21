9ec5df2fa701aa480c7935605e0817dd64b4224f
tianzj <tianzhijin@joiest.com>
2018-12-21 10:29:15 +0800

    修改走威能的接口

--- jpf-market-api/src/main/java/com/joiest/jpf/market/api/controller/OrdersController.java
+++ jpf-market-api/src/main/java/com/joiest/jpf/market/api/controller/OrdersController.java
@@ -1820,10 +1820,8 @@ public class OrdersController {
                             sbf.append("\n请求下游地址："+orderInfo.getNotifyUrl());
                             sbf.append("\n\t请求下游参数："+JSONObject.fromObject(sendParam).toString());
                             //发起下游请求
-
                             logger.info("回答站远start");
                             //OkHttpUtils.postForm(orderInfo.getNotifyUrl(),sendParam);
-
                         }
                     }else{
                         ShopOrderInterfaceInfo orderInfo = shopOrderInterfaceServiceFacade.getOrderByOrderNo(job.get("outOrderId").toString());
